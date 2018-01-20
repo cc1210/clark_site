70b9bd9a18eec8a320c68e695fead33dc5da6521
Chris Cumby <chapfan42@gmail.com>
2018-01-20 11:33:04 -0800

    Changing the layout of the homepage.  Added the arrow svg

--- scripts/header.js
+++ scripts/header.js
@@ -1,14 +1,15 @@
 var headArr = ['Home', 'Bio', 'Music', 'Tour', 'Gallery', 'Contact'];
 
-var headTbl = document.getElementById('header');
-var headDiv = document.createElement('div');
-var headClass = document.createAttribute('class');
+//var headTbl = document.getElementById('header');
+//var headDiv = document.createElement('div');
+var headDiv = document.getElementById('headerDiv');
+//var headClass = document.createAttribute('class');
 var headEl = document.getElementsByTagName('head').item(0);
-var parent = headTbl.parentNode;
+//var parent = headTbl.parentNode;
 
 // set the class of the new div
-headClass.value = 'headerDiv';
-headDiv.setAttributeNode(headClass);
+//headClass.value = 'headerDiv';
+//headDiv.setAttributeNode(headClass);
 
 function addFont(font) {
     let link = document.createElement('link');
@@ -26,16 +27,21 @@ function addElement(parentElement, newElement) {
     parentElement.appendChild(newElement);
 }
 
-addElement(headEl, addFont("Lobster")); 
+addElement(headEl, addFont("Lobster"));
+addElement(headEl, addFont("Ropa+Sans"));
 
 // set the div wrapper as a replacement child for table
-parent.replaceChild(headDiv, headTbl);
+//parent.replaceChild(headDiv, headTbl);
 
 // set table as the child of the div container
-headDiv.appendChild(headTbl);
+//headDiv.appendChild(headTbl);
 
 headArr.forEach(function(element) {
-    let tbleH = document.createElement('th');
+//    let tbleH = document.createElement('th');
+    let anchorDiv = document.createElement('div');
+    let anchorSpan = document.createElement('span');
+    let anchorDivClass = document.createAttribute('class');
+    let anchorSpanId = document.createAttribute('id');
     let anchor = document.createElement('a');
     let menuItem = element.toUpperCase();
     let table = document.getElementById('header');
@@ -43,13 +49,23 @@ headArr.forEach(function(element) {
     let aClass = document.createAttribute('class');
     let text = document.createTextNode(menuItem);
     
-    table.appendChild(tbleH);
-    tbleH.appendChild(anchor);
+//    table.appendChild(tbleH);
+//    tbleH.appendChild(anchor);
+    // assigning the child divs for each header anchor element
+    headDiv.appendChild(anchorDiv);    
+    anchorDivClass.value = menuItem.toLowerCase();
+    anchorDiv.setAttributeNode(anchorDivClass);
     
+    // assigning span a class
+    anchorSpanId.value = 'span' + menuItem.toLocaleLowerCase();
+    anchorSpan.setAttributeNode(anchorSpanId);
+    
+    // setting header anchor elements inside each child div and a span
     attr.value = 'clark' + element + '.html';
     aClass.value = 'navlink';
     anchor.setAttributeNode(attr);
     anchor.setAttributeNode(aClass);
     anchor.appendChild(text);
-    
+    anchorDiv.appendChild(anchorSpan);
+    anchorSpan.appendChild(anchor);
 });
--- scripts/headerActions.js
+++ scripts/headerActions.js
@@ -1,6 +1,8 @@
 var urlLink = document.baseURI;
 var urlArr = urlLink.split('\/');
 var headLinkArr = document.getElementsByClassName('navlink');
+var imgSvg = "<img src=\"stylesheets\\Images\\header\\menuArrow.svg\" alt=\"currentMenuItemIcon\" class=\"currentHeadLink\">";
+var headArr = ['home', 'bio', 'music', 'tour', 'gallery', 'contact'];
 
 // Get the last part of the URL
 function getPageName(arr) {
@@ -12,8 +14,14 @@ let currentPage = getPageName(urlArr);
 // Compare the header links to the current page and set a style for the link that matches
 for (step = 0; step < headLinkArr.length; step ++) {
     var headElem = headLinkArr.item(step);
-    console.log(headElem);
+    var spanElem = document.getElementById('span' + headArr[step]);
+    var spanImgElem = document.createElement('span');
+    var spanImgElemId = document.createAttribute('id');
+    
     if ( currentPage === headElem.getAttribute('href')) {
         headElem.setAttribute('id', 'currentPage');
+        spanImgElem.setAttribute('id', 'currentPageImg');
+        spanElem.insertAdjacentElement('beforebegin', spanImgElem);
+        spanImgElem.insertAdjacentHTML('afterbegin', imgSvg);
     }
 }
